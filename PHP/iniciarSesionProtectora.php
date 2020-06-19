683f953aa3f3141a900abd3f8a9ea3e4ffce02db
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:00:23 +0200

    Update iniciarSesionProtectora.php

--- PHP/iniciarSesionProtectora.php
+++ PHP/iniciarSesionProtectora.php
@@ -12,7 +12,7 @@
 		$pro="";
 	}
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd')or die('no se pudo conectar'.mysqli_error($con));
+    $con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
     $con->set_charset("utf8");
 	
 	session_start();
