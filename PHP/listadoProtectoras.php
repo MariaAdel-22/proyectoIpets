6857598855d3c7330d5a4588f46ae4c85b846b8a
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:02:45 +0200

    Update listadoProtectoras.php

--- PHP/listadoProtectoras.php
+++ PHP/listadoProtectoras.php
@@ -9,7 +9,7 @@
 	
 	$nombreU=$_SESSION['nombre'];
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 
 	$consulta="SELECT p.NOMBRE,p.IDENTIFICADOR,p.IMAGEN FROM protectora p,usuario u WHERE u.NOMBRE='$nombreU' AND u.LOCALIDAD=p.LOCALIDAD";
 	$con->set_charset("utf8");
