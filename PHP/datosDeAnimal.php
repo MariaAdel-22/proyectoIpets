67991fd6fcc1b7f1b3b88380e02320d3e7648093
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:41:24 +0200

    Update datosDeAnimal.php

--- PHP/datosDeAnimal.php
+++ PHP/datosDeAnimal.php
@@ -7,7 +7,7 @@
 	$id=$_SESSION['id'];
 	$datos=array();
 		
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 
 	$consulta="SELECT ID,NOMBRE,ESPECIE,EDAD,GENERO,RAZA,FECHANACIMIENTO,TIEMPOINYECCION,PESO,IMAGEN FROM animal WHERE ID='$id'";
