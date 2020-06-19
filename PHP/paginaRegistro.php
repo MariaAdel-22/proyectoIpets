51d4c060dc45f2821d7bbac6925206880cda823b
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:04:52 +0200

    Update paginaRegistro.php

--- PHP/paginaRegistro.php
+++ PHP/paginaRegistro.php
@@ -9,7 +9,7 @@
 	
 	$datoT=$_SESSION['dat'];
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	
 	
 	if(($datoTabla != "")&&($datoT=="")){
