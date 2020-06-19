d9e257ce7700bed48a629ceee009d87280b09216
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:42:09 +0200

    Update datosDeCuenta.php

--- PHP/datosDeCuenta.php
+++ PHP/datosDeCuenta.php
@@ -10,7 +10,7 @@
 	$nombreU=$_SESSION['nombre'];
 	$nombreP=$_SESSION['ident'];
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	if(isset($nombreU)){
