49a7ac8b12ac9ed8217b9c892f2d0fbbbaf70fa1
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:03:10 +0200

    Update modificarDatosAnimal.php

--- PHP/modificarDatosAnimal.php
+++ PHP/modificarDatosAnimal.php
@@ -3,7 +3,7 @@
 	header('Content-Type: text/html; charset=UTF-8');
 	session_start();
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd')or die('No se pudo establecer conexion.'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$id=$_SESSION['id'];
