29d333b7dabf924be0cbdea35f9cea8f51764ad1
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:39:03 +0200

    Update adoptados.php

--- PHP/adoptados.php
+++ PHP/adoptados.php
@@ -14,7 +14,7 @@
 	
 	$fecha=date("d")."/".date("m")."/".date("Y")." a las ".date("H")." : ".date("i");
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$consulta="SELECT u.DNI,a.ID,p.IDENTIFICADOR FROM usuario u, animal a, disponibles d,protectora p WHERE u.NOMBRE='$nombre' AND a.ID='$id' AND a.NOMBRE=d.ANIMAL AND p.NOMBRE=d.PROTECTORA ORDER BY DNI ASC";
