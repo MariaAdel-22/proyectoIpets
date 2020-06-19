84e534e9c56d8467e392805b79def4f7f73710c5
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:04:18 +0200

    Update pagina6.php

--- PHP/pagina6.php
+++ PHP/pagina6.php
@@ -7,7 +7,7 @@
 	
 	$nombreU=$_SESSION['nombre'];
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$consulta="SELECT a.NOMBRE,a.IMAGEN,a.ID FROM animal a,usuario u,seleccionados s WHERE u.NOMBRE='$nombreU' AND u.NOMBRE=s.USUARIO AND a.NOMBRE=s.ANIMAL";
