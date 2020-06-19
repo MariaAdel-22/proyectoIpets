250ee8225fd26e428a5f4fc92f45e91e498d6b5a
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:07:11 +0200

    Update seleccionados.php

--- PHP/seleccionados.php
+++ PHP/seleccionados.php
@@ -8,7 +8,7 @@
 	$nombre=$_SESSION['nombre'];
 	$id=$_POST['id1'];
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	 
 	$consulta="SELECT u.NOMBRE 'nom_u',a.NOMBRE 'nom_a',d.PROTECTORA FROM usuario u,animal a,disponibles d WHERE u.NOMBRE='$nombre' 
