2e3e5b0e118787e530882e182d7bbf122bd66f77
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:42:46 +0200

    Update datosPag2.php

--- PHP/datosPag2.php
+++ PHP/datosPag2.php
@@ -1,6 +1,6 @@
 <?php
 		
-		$con=mysqli_connect("localhost","root","") or die('No se pudo conectar'.mysqli_error($con));
+		$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 		mysqli_select_db($con,"ipetsbbdd")or die('No se pudo conectar a la base de datos');
 		
 		$consulta1="SELECT IDENTIFICADOR FROM protectora";
