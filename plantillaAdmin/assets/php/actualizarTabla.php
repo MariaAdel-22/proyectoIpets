2764342ef5c86204b285624a735e11f5b2018a6c
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:08:54 +0200

    Update actualizarTabla.php

--- plantillaAdmin/assets/php/actualizarTabla.php
+++ plantillaAdmin/assets/php/actualizarTabla.php
@@ -4,7 +4,7 @@
 	 
 	error_reporting(0);
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$datos=$_POST['datos'];
