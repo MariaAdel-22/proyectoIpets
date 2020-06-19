daf7b8076bde16393668cf2ca8ee21fb95ab6c26
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:14:47 +0200

    Update todasLasTablas.php

--- plantillaAdmin/assets/php/todasLasTablas.php
+++ plantillaAdmin/assets/php/todasLasTablas.php
@@ -3,7 +3,7 @@
 	error_reporting(0);
 	header('Content-Type: text/html; charset=UTF-8');
 	
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	$sql= "SHOW TABLES FROM ipetsbbdd WHERE Tables_in_ipetsbbdd<>'adoptados'";
 	$resultado = mysqli_query($con,$sql);
