e9f218b88ce80a7d9972dd0813b6e0f7e83ffcc2
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:43:16 +0200

    Update desSeleccionado.php

--- PHP/desSeleccionado.php
+++ PHP/desSeleccionado.php
@@ -10,7 +10,7 @@
 	
 	$id=$_POST['id1'];
 
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$consulta2="DELETE FROM seleccionados WHERE USUARIO='$nombre' AND ANIMAL=(SELECT NOMBRE FROM animal WHERE ID='$id')";
