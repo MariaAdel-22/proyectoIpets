101879d937fb381536bd40e50c5bab2c5cd051c3
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:05:53 +0200

    Update pasoDatosProtectora.php

--- PHP/pasoDatosProtectora.php
+++ PHP/pasoDatosProtectora.php
@@ -9,7 +9,7 @@
 		
 		$pro=$_POST['protectora'];
 		
-		$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+		$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 		$con->set_charset("utf8");
 		
 		$consulta="SELECT IDENTIFICADOR FROM protectora WHERE NOMBRE='$pro'";
