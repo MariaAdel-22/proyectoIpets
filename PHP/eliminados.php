c65aa141555f1a3ca9816333a2d71121f6ebf411
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 17:58:20 +0200

    Update eliminados.php

--- PHP/eliminados.php
+++ PHP/eliminados.php
@@ -11,7 +11,7 @@
 	
 	$id=$_SESSION['id2'];
 
-	$con=mysqli_connect('localhost','root','','ipetsbbdd') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$consulta2="DELETE p1,p2,p3 FROM seleccionados p1,disponibles p2,animal p3 WHERE p1.USUARIO='$nombre' AND p3.ID='$id' AND p1.ANIMAL=p3.NOMBRE AND p2.ANIMAL=p3.NOMBRE";
