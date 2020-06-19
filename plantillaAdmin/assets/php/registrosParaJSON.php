bd0200df37a8285f048f929128875d20d0c0e3be
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-19 18:14:01 +0200

    Update registrosParaJSON.php

--- plantillaAdmin/assets/php/registrosParaJSON.php
+++ plantillaAdmin/assets/php/registrosParaJSON.php
@@ -2,7 +2,7 @@
 
 	header("Content-Type: application/json; charset=UTF-8");
 		 
-	 $con = mysqli_connect('localhost','root','','ipetsbbdd');
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 
 	$con->query("SET NAMES 'utf8'");
 						                
