34a12fa2167daa905a8f50c1270dd8f35e8aa90d
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:35:34 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -10,14 +10,12 @@
 	$nombreU=$_SESSION['nombre'];
 	$nombreP=$_SESSION['ident'];
 	
-	//$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39');
 	$con->set_charset("utf8");
 	
 	$dat=$_POST['datos'];
 
-	//$consulta="GRANT SELECT, INSERT, DELETE ON heroku_0c87bc892272e39 TO username@'be2cf74825313e' IDENTIFIED BY 'e459b73e'";
-	$consulta="GRANT ALL heroku_0c87bc892272e39 TO username@'be2cf74825313e' IDENTIFIED BY 'e459b73e'";
-	mysqli_query($con,$consulta);
+	/*$consulta="GRANT ALL heroku_0c87bc892272e39 TO username@'be2cf74825313e' IDENTIFIED BY 'e459b73e'";
+	mysqli_query($con,$consulta);*/
 		
 	/*$consulta2="SELECT DNI FROM usuario";
 	$res=mysqli_query($con,$consulta2);
