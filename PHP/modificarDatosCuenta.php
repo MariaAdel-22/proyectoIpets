c839611cb568fc82fc6119d0a172c6d1d9b17e86
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:08:01 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -1,7 +1,7 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
 	
-	error_reporting(0);
+	error_reporting(E_ALL);
 	
 	include 'pasoDatosDeUsuario.php';
 	include 'pasoDatosProtectora.php';
