b564bb9e76028a91e56fdffb3aabf04207f4f545
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 13:25:51 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -1,7 +1,7 @@
 <?php
 	header('Content-Type: text/html; charset=UTF-8');
 	
-	//error_reporting(0);
+	error_reporting(0);
 	
 	include 'pasoDatosDeUsuario.php';
 	include 'pasoDatosProtectora.php';
@@ -10,15 +10,14 @@
 	$nombreP=$_SESSION['ident'];
 	
 	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39');
-	//$con->set_charset("utf8");
+	$con->set_charset("utf8");
 	
 	$dat=$_POST['datos'];
 
 	$consulta="GRANT SELECT, INSERT, DELETE ON heroku_0c87bc892272e39 TO username@'be2cf74825313e' IDENTIFIED BY 'e459b73e'";
-	//$consulta="GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP  ON heroku_0c87bc892272e39.* TO 'be2cf74825313e'@'us-cdbr-east-05.cleardb.net';
 	mysqli_query($con,$consulta);
 		
-	$consulta2="SELECT DNI FROM usuario";
+	/*$consulta2="SELECT DNI FROM usuario";
 	$res=mysqli_query($con,$consulta2);
 	$fila=mysqli_fetch_assoc($res);
 
@@ -28,8 +27,89 @@
 			$fila=mysqli_fetch_assoc($res);
 			echo $nom;
 		}
+	}*/
+	
+	$cont=0;
+	$nombeT="";
+	
+	$imag=$_FILES["dato"];
+	$nombre=$imag["name"];
+	$ruta_prov=$imag["tmp_name"];
+	
+	foreach($dat as $clave => $valor){
+	
+		foreach($valor as $clave2 => $valor2){
+			
+	
+			foreach($valor2 as $clave3 => $valor3){
+				
+				$nombreT=$clave;
+				
+				foreach($valor3 as $clave4 => $valor4){
+						
+					if($nombreU!=""){
+						
+						$nombreP="";
+						
+						if(!empty($valor4)){
+							
+							$cont++;
+							$consulta2="UPDATE $nombreT SET $clave2='".mb_convert_encoding($valor4,'UTF-8')."' WHERE $clave2='$clave3' AND NOMBRE='$nombreU'";
+							mysqli_query($con,$consulta2);
+						}
+						
+					}
+					
+					if($nombreP!=""){
+						
+						$nombreU="";
+						
+						if(!empty($valor4)){
+							
+							$cont++;
+							
+							$consulta2="UPDATE $nombreT SET $clave2='".mb_convert_encoding($valor4,'UTF-8')."' WHERE $clave2='$clave3' AND IDENTIFICADOR='$nombreP'";
+							mysqli_query($con,$consulta2);
+
+						}
+
+					}
+				}
+			}
+		}
 	}
 	
+	if($nombreU!=""){
+		
+		if(isset($imag)){
+							
+			$carpeta="../images/";
+
+			$src=$carpeta.$nombre;
+			
+			move_uploaded_file($ruta_prov,$src);
+			
+			$consulta3="UPDATE usuario SET IMAGEN = '".mb_convert_encoding($nombre,'UTF-8')."' WHERE NOMBRE='$nombreU'";
+			mysqli_query($con,$consulta3);
+			echo $src;
+		}
+	}
+	
+	if($nombreP!=""){
+		
+		if(isset($imag)){
+							
+			$carpeta="../images/PROTECTORAS/";
+
+			$src=$carpeta.$nombre;
+			
+			move_uploaded_file($ruta_prov,$src);
+			
+			$consulta3="UPDATE protectora SET IMAGEN = '".mb_convert_encoding($nombre,'UTF-8')."' WHERE IDENTIFICADOR='$nombreP'";
+			mysqli_query($con,$consulta3);
+			echo $src;
+		}
+	}
 	
 mysqli_close($con);
 
