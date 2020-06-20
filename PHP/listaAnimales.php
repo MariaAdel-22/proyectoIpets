ef84ad13ca3183d3f967b284bd9f98ff22cba5e5
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:02:57 +0200

    Update listaAnimales.php

--- PHP/listaAnimales.php
+++ PHP/listaAnimales.php
@@ -3,12 +3,13 @@
   header('Content-Type: text/html; charset=UTF-8');
   
   session_start();
-  $con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+  require 'conexion.php';
+
   $con->set_charset("utf8");
   
-   $especie=$_SESSION['lis1'];
-   $raza=$_SESSION['lis2'];
-   $edad=$_SESSION['lis3'];
+  $especie=$_SESSION['lis1'];
+  $raza=$_SESSION['lis2'];
+  $edad=$_SESSION['lis3'];
   
 	switch($especie){
 	 
