c5db491af2f089f87846f01aaa552860c2682c01
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:55:03 +0200

    Update desSeleccionado.php

--- PHP/desSeleccionado.php
+++ PHP/desSeleccionado.php
@@ -2,17 +2,16 @@
 	
 	header('Content-Type: text/html; charset=UTF-8');
 	
-	error_reporting(0);
+	error_reporting(E_ALL);
 	
 	include 'pasoDatosDeUsuario.php';
+	require 'conexion.php';
+
+	$con->set_charset("utf8");
 
 	$nombre=$_SESSION['nombre'];
-	
 	$id=$_POST['id1'];
 
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
-	$con->set_charset("utf8");
-	
 	$consulta2="DELETE FROM seleccionados WHERE USUARIO='$nombre' AND ANIMAL=(SELECT NOMBRE FROM animal WHERE ID='$id')";
 	
 	$res2=mysqli_query($con,$consulta2)or die('consulta fallida'.mysqli_error($con));
