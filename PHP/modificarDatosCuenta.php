727221441053ba871e1fe94c413ab1a736534105
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 12:30:54 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -10,7 +10,7 @@
 	$nombreU=$_SESSION['nombre'];
 	$nombreP=$_SESSION['ident'];
 	
-	/*$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
 	
 	$dat=$_POST['datos'];
@@ -18,8 +18,8 @@
 	$consulta "SELECT NOMBRE FROM usuario WHERE NOMBRE='$nombreU'";
 	$res=mysqli_query($con,$consulta);
 	$fila=mysqli_fetch_array($res);
-	echo $fila[0];*/
-	echo "Si entro";
+	echo $fila[0];
+
 	/*$cont=0;
 	$nombeT="";
 	
