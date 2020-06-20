b5f9721eb2ac9e5094972f483fa4749e42957309
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 12:09:24 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -1,6 +1,6 @@
 <?php
 	
-	header('Content-Type: text/html; charset=UTF-8');
+	/*header('Content-Type: text/html; charset=UTF-8');
 	
 	error_reporting(0);
 	
@@ -10,13 +10,13 @@
 	$nombreU=$_SESSION['nombre'];
 	$nombreP=$_SESSION['ident'];
 	
-	/*$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	mysqli_select_db($con,"heroku_0c87bc892272e39")or die('No se pudo conectar a la base de datos');
 	$con->set_charset("utf8");*/
 	
 	$dat=$_POST['datos'];
 
-	$cont=0;
+	/*$cont=0;
 	$nombeT="";
 	
 	$imag=$_FILES["dato"];
@@ -98,6 +98,7 @@
 		}
 	}
 	
-mysqli_close($con);
+mysqli_close($con);*/
+print_r($dat);
 
 ?>
