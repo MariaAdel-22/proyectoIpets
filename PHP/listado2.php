9725ba0a82d6eb9827a8df391c1ae22a27cfa2ea
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:04:16 +0200

    Update listado2.php

--- PHP/listado2.php
+++ PHP/listado2.php
@@ -3,8 +3,7 @@
 	header('Content-Type: text/html; charset=UTF-8');
 	
 	session_start();
-	
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	require 'conexion.php';
 	$con->set_charset("utf8");
 	
 	$_SESSION['resp1']  = $_POST['resp1'];
@@ -16,15 +15,15 @@
 		
 		case "TODOS":
 		
-			$consulta="SELECT RAZA FROM animal ORDER BY RAZA ASC";
-			$res=mysqli_query($con,$consulta) or die('Consulta fallida'.mysqli_error($con));
+			$consulta1="SELECT RAZA FROM animal ORDER BY RAZA ASC";
+			$res=mysqli_query($con,$consulta1) or die('Consulta fallida'.mysqli_error($con));
 		
 		break;
 		
 		default:
 		
-			$consulta="SELECT RAZA FROM animal WHERE ESPECIE='$resp' ORDER BY RAZA ASC";
-			$res=mysqli_query($con,$consulta) or die('Consulta fallida'.mysqli_error($con));
+			$consulta2="SELECT RAZA FROM animal WHERE ESPECIE='$resp' ORDER BY RAZA ASC";
+			$res=mysqli_query($con,$consulta2) or die('Consulta fallida'.mysqli_error($con));
 		
 		break;
 		
