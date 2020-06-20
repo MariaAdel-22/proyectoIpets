c89e68100145f203f2e79df4cdb99dc9f50f7053
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:47:57 +0200

    Update animalesFavoritos.php

--- PHP/animalesFavoritos.php
+++ PHP/animalesFavoritos.php
@@ -5,15 +5,14 @@
 	error_reporting(0);
 	
 	include 'pasoDatosProtectora.php';
+	require 'conexion.php';
 
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
 	$con->set_charset("utf8");
-	
 	$ident=$_SESSION['ident'];
 	
-	$consulta="SELECT u.DNI,s.ANIMAL,u.IMAGEN 'imag_usu',a.IMAGEN 'imag_anim',a.NOMBRE FROM seleccionados s,usuario u,animal a,disponibles d WHERE s.USUARIO=u.NOMBRE AND s.ANIMAL=a.NOMBRE AND d.ANIMAL=a.NOMBRE AND d.PROTECTORA=(SELECT NOMBRE FROM protectora WHERE IDENTIFICADOR='$ident') ORDER BY s.ANIMAL,s.USUARIO ASC";
+	$consulta2="SELECT u.DNI,s.ANIMAL,u.IMAGEN 'imag_usu',a.IMAGEN 'imag_anim',a.NOMBRE FROM seleccionados s,usuario u,animal a,disponibles d WHERE s.USUARIO=u.NOMBRE AND s.ANIMAL=a.NOMBRE AND d.ANIMAL=a.NOMBRE AND d.PROTECTORA=(SELECT NOMBRE FROM protectora WHERE IDENTIFICADOR='$ident') ORDER BY s.ANIMAL,s.USUARIO ASC";
 		
-	$res=mysqli_query($con,$consulta) or die('Consulta fallida'.mysqli_error($con));
+	$res=mysqli_query($con,$consulta2) or die('Consulta fallida'.mysqli_error($con));
 	$fila=mysqli_fetch_assoc($res);
 	
 	while($fila){
