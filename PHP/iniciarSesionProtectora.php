2f8da104ab825a6fe77cce600c0f4431056d5f78
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:01:42 +0200

    Update iniciarSesionProtectora.php

--- PHP/iniciarSesionProtectora.php
+++ PHP/iniciarSesionProtectora.php
@@ -5,15 +5,15 @@
 	error_reporting(0);
 	
 	include 'pasoDatosPag8_1';
+	require 'conexion.php';
+
+	$con->set_charset("utf8");
 	
 	if(isset($_SESSION['protectora'])){
 		
 		unset($_SESSION['protectora']);
 		$pro="";
 	}
-	
-    $con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
-    $con->set_charset("utf8");
 	
 	session_start();
 	
@@ -22,9 +22,9 @@
     $identificador=$_POST['ident'];
     $contra=$_POST['pwd'];
 
-    $consulta="SELECT IDENTIFICADOR,CONTRASENIA FROM protectora WHERE IDENTIFICADOR='$identificador'";
+    $consulta1="SELECT IDENTIFICADOR,CONTRASENIA FROM protectora WHERE IDENTIFICADOR='$identificador'";
 
-    $res=mysqli_query($con,$consulta)or die('Consulta fallida'.mysqli_error($con));
+    $res=mysqli_query($con,$consulta1)or die('Consulta fallida'.mysqli_error($con));
     $fila=mysqli_fetch_assoc($res);
 
     while($fila){
