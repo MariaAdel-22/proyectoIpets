a69e141445e7897fbda46b273b2aaafcd415abb9
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:48:30 +0200

    Update adoptados.php

--- PHP/adoptados.php
+++ PHP/adoptados.php
@@ -16,9 +16,9 @@
 	
 	$fecha=date("d")."/".date("m")."/".date("Y")." a las ".date("H")." : ".date("i");
 	
-	$consulta="SELECT u.DNI,a.ID,p.IDENTIFICADOR FROM usuario u, animal a, disponibles d,protectora p WHERE u.NOMBRE='$nombre' AND a.ID='$id' AND a.NOMBRE=d.ANIMAL AND p.NOMBRE=d.PROTECTORA ORDER BY DNI ASC";
+	$consulta2="SELECT u.DNI,a.ID,p.IDENTIFICADOR FROM usuario u, animal a, disponibles d,protectora p WHERE u.NOMBRE='$nombre' AND a.ID='$id' AND a.NOMBRE=d.ANIMAL AND p.NOMBRE=d.PROTECTORA ORDER BY DNI ASC";
 	
-	$res=mysqli_query($con,$consulta) or die('Consulta fallida'.mysqli_error($con));
+	$res=mysqli_query($con,$consulta2) or die('Consulta fallida'.mysqli_error($con));
 	$fila=mysqli_fetch_assoc($res);
 	
 	while($fila){
@@ -29,9 +29,9 @@
 		while($fila && $dni==$fila['DNI'] ){
 			
 			$ide=$fila['ID'];
-			$consulta2="INSERT into adoptados (USUARIO,ANIMAL,PROTECTORA,HORA) values ('".mb_convert_encoding($dni,'UTF-8')."','".mb_convert_encoding($ide,'UTF-8')."','".mb_convert_encoding($prot,'UTF-8')."','".mb_convert_encoding($fecha,'UTF-8')."')";
+			$consulta3="INSERT into adoptados (USUARIO,ANIMAL,PROTECTORA,HORA) values ('".mb_convert_encoding($dni,'UTF-8')."','".mb_convert_encoding($ide,'UTF-8')."','".mb_convert_encoding($prot,'UTF-8')."','".mb_convert_encoding($fecha,'UTF-8')."')";
 			
-			mysqli_query($con,$consulta2)or die('Segunda consulta fallida'.mysqli_error($con));
+			mysqli_query($con,$consulta3)or die('Segunda consulta fallida'.mysqli_error($con));
 				
 			mysqli_fetch_assoc($res);
 
