d8d8c39268d52e1ac7b35d80d720889cfc53943f
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:14:13 +0200

    Update usuario.php

--- PHP/usuario.php
+++ PHP/usuario.php
@@ -2,7 +2,7 @@
 	
 	header('Content-Type: text/html; charset=UTF-8');
 	
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39')or die('no se pudo conectar'.mysqli_error($con));
+	require 'conexion.php';
 	$con->set_charset("utf8");
 	 
 	session_start();
@@ -12,10 +12,10 @@
 	$nombre=$_POST['nombre'];
 	$contrasenia=$_POST['pwd'];
 	
-	$consulta="SELECT NOMBRE,CONTRASENIA
+	$consulta1="SELECT NOMBRE,CONTRASENIA
 			   FROM usuario WHERE NOMBRE='$nombre'";
 
-	$res=mysqli_query($con,$consulta)or die('Consulta fallida'.mysqli_error($con));
+	$res=mysqli_query($con,$consulta1)or die('Consulta fallida'.mysqli_error($con));
 	$fila=mysqli_fetch_assoc($res);
 	
 	while($fila){
