bccc18dff8935be3b0189883299dfe1daccde7ce
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:31:23 +0200

    Update modificarUsuario.js

--- js/modificarUsuario.js
+++ js/modificarUsuario.js
@@ -433,8 +433,8 @@ $(document).ready(function(){
 					contentType:false,
 					success:function(resp){
 						
-						//window.history.back();
-						console.log(resp);
+						window.history.back();
+						//console.log(resp);
 					},
 					error:function(){
 						
