e6fc26f655ca841d7f20b3f8acce1efafb3144d2
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 15:35:24 +0200

    Update conexion.php

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,5 +1,6 @@
 <?php
 
 $con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida.'.mysqli_error($con));
-
+$consulta="GRANT ALL heroku_0c87bc892272e39 TO username@'be2cf74825313e' IDENTIFIED BY 'e459b73e'";
+mysqli_query($con,$consulta);
 ?>
