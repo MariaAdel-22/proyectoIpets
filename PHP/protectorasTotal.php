5861a3825fa530b34dcfca9a84484f77a61ae246
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:11:56 +0200

    Update protectorasTotal.php

--- PHP/protectorasTotal.php
+++ PHP/protectorasTotal.php
@@ -2,11 +2,12 @@
 	
 	header('Content-Type: text/html; charset=UTF-8');
 
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	require 'conexion.php';
 	$con->set_charset("utf8");
-	$consulta="SELECT IDENTIFICADOR,NOMBRE,LOCALIDAD,IMAGEN FROM protectora";
+
+	$consulta1="SELECT IDENTIFICADOR,NOMBRE,LOCALIDAD,IMAGEN FROM protectora";
 	
-    $res=mysqli_query($con,$consulta)or die('Consulta fallida'.mysqli_error($con));
+    $res=mysqli_query($con,$consulta1)or die('Consulta fallida'.mysqli_error($con));
     $fila=mysqli_fetch_assoc($res);
 		
 	while($fila){
