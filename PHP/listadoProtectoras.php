9848966debb82e472052d353b1cec5f04ffc8f3a
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:06:00 +0200

    Update listadoProtectoras.php

--- PHP/listadoProtectoras.php
+++ PHP/listadoProtectoras.php
@@ -6,14 +6,14 @@
 	error_reporting(0);
 	
 	include 'pasoDatosDeUsuario.php';
-	
+	require 'conexion.php';
+	$con->set_charset("utf8");
+
 	$nombreU=$_SESSION['nombre'];
 	
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	$consulta1="SELECT p.NOMBRE,p.IDENTIFICADOR,p.IMAGEN FROM protectora p,usuario u WHERE u.NOMBRE='$nombreU' AND u.LOCALIDAD=p.LOCALIDAD";
 
-	$consulta="SELECT p.NOMBRE,p.IDENTIFICADOR,p.IMAGEN FROM protectora p,usuario u WHERE u.NOMBRE='$nombreU' AND u.LOCALIDAD=p.LOCALIDAD";
-	$con->set_charset("utf8");
-	$res=mysqli_query($con,$consulta) or die('Consulta fallida'.mysqli_error($con));
+	$res=mysqli_query($con,$consulta1) or die('Consulta fallida'.mysqli_error($con));
 	$fila=mysqli_fetch_assoc($res);
 	
 	while($fila){
