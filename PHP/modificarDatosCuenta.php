054c80cdc5fcd44028dccccadca2b972027611c8
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 12:22:32 +0200

    Update modificarDatosCuenta.php

--- PHP/modificarDatosCuenta.php
+++ PHP/modificarDatosCuenta.php
@@ -10,12 +10,15 @@
 	$nombreU=$_SESSION['nombre'];
 	$nombreP=$_SESSION['ident'];
 	
-	/*$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
-	mysqli_select_db($con,"heroku_0c87bc892272e39")or die('No se pudo conectar a la base de datos');
-	$con->set_charset("utf8");*/
+	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	$con->set_charset("utf8");
 	
 	$dat=$_POST['datos'];
 
+	$consulta "SELECT NOMBRE FROM usuario WHERE NOMBRE='$nombreU'";
+	$res=mysqli_query($con,$consulta);
+	$fila=mysqli_fetch_array($res);
+	echo $fila[0];
 	/*$cont=0;
 	$nombeT="";
 	
@@ -96,9 +99,8 @@
 			mysqli_query($con,$consulta3);
 			echo $src;
 		}
-	}
+	}*/
 	
-mysqli_close($con);*/
-echo $nombreU." - ".$nombreP;
+mysqli_close($con);
 
 ?>
