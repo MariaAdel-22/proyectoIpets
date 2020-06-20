c8f19bbe2265f519dd001b971e28e0917c419d62
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-20 16:22:20 +0200

    Update datosDeAdmin.php

--- plantillaAdmin/assets/php/datosDeAdmin.php
+++ plantillaAdmin/assets/php/datosDeAdmin.php
@@ -3,11 +3,10 @@
 	header('Content-Type: text/html; charset=UTF-8');
 	
 	include 'guardarDatosSesionAdmin.php';
-	
-	$nombre=$_SESSION['nombre'];
-	
-	$con=mysqli_connect('us-cdbr-east-05.cleardb.net','be2cf74825313e','e459b73e','heroku_0c87bc892272e39') or die('Conexion fallida'.mysqli_error($con));
+	require '../../../PHP/conexion.php';
 	$con->set_charset("utf8");
+
+	$nombre=$_SESSION['nombre'];
 	
 	$dat=$_POST['nom'];
 	
@@ -18,8 +17,8 @@
 			if(!empty($valor1)){
 				
 
-				$consulta="UPDATE administradores SET $clave='".mb_convert_encoding($valor1,'UTF-8')."' WHERE NOMBRE='$nombre'";
-				mysqli_query($con,$consulta);
+				$consulta1="UPDATE administradores SET $clave='".mb_convert_encoding($valor1,'UTF-8')."' WHERE NOMBRE='$nombre'";
+				mysqli_query($con,$consulta1);
 			}
 
 		}
