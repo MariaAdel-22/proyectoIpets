d4f5fe13ef8cb98f4b49e9ee528f621975d950eb
Maria Adela <65348904+MariaAdel-22@users.noreply.github.com>
2020-06-21 17:45:29 +0200

    Update seleccionados.php

--- PHP/seleccionados.php
+++ PHP/seleccionados.php
@@ -10,13 +10,13 @@
 	$nombre=$_SESSION['nombre'];
 	$id=$_POST['id1'];
 	 
-	$consulta1="SELECT u.NOMBRE 'nom_u',a.NOMBRE 'nom_a',d.PROTECTORA FROM usuario u,animal a,disponibles d WHERE u.NOMBRE='$nombre' 
+	$consulta1="SELECT u.NOMBRE 'nom_u',u.DNI,a.NOMBRE 'nom_a',a.ID,d.PROTECTORA,d.IDENTIFICADOR FROM usuario u,animal a,disponibles d WHERE u.NOMBRE='$nombre' 
 	AND a.NOMBRE=(SELECT NOMBRE FROM animal WHERE ID='$id') AND a.NOMBRE=d.ANIMAL";
 	
 	$res=mysqli_query($con,$consulta1) or die('Consulta fallida'.mysqli_error($con));
 	$fila=mysqli_fetch_assoc($res);
 	
-	while($fila){
+	/*while($fila){
 		
 		$dni=$fila['nom_u'];
 		$prot=$fila['PROTECTORA'];
@@ -29,6 +29,33 @@
 			mysqli_query($con,$consulta2)or die('Segunda consulta fallida'.mysqli_error($con));
 
 			$fila=mysqli_fetch_assoc($res);
+		}
+		
+	}*/
+
+	while($fila){
+		
+		$nom=$fila['nom_u'];
+		$dni=$fila['DNI'];
+		
+		while($fila && $dni==$fila['DNI'] ){
+			
+			$nom2=$fila['nom_a'];
+			$ide=$fila['ID'];
+			
+			while($fila && $dni==$fila['DNI'] && $ide==$fila['ID']){
+				
+				$nom3=$fila['PROTECTORA'];
+				$ident=$fila['IDENTIFICADOR'];
+				
+				while($fila && $dni==$fila['DNI'] && $ide==$fila['ID'] && $ident==$fila['IDENTIFICADOR']){
+					
+					$consulta2="INSERT into seleccionados (ID,ANIMAL,DNI,USUARIO,IDENTIFICADOR,PROTECTORA) values ('".$nom."','".$dni."','".$nom2."','".$ide."','".$nom3.'"','".$ident."')";
+					mysqli_query($con,$consulta2)or die('Segunda consulta fallida'.mysqli_error($con));
+					
+					$fila=mysqli_fetch_assoc($res);
+				}
+			}
 		}
 		
 	}
